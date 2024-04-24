80ad61479c151dfc111bb009bf42f511cbf6029a
diegohenao <dadegad@gmail.com>
2024-04-24 15:14:32 -0500

    actualizacion abril 24 2024

---
+++ resources/views/docente.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+@section('titulo','docente')   
+@section('contenido')
+<h1 class="text-3xl text-green-600 font-bold underline">
+    <center>PORTAL DOCENTE</center>
+  </h1>
+<h1 class="text-6xl text-green-600 font-bold underline">
+    <center>SITIO EN CONSTRUCCION</center>
+  </h1>
+
+@endsection
+
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('titulo','LAESPERANZA')</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+   <header>
+    @include('layouts.navbar') 
+   </header>
+   <main> 
+    @yield('contenido')
+   </main>
+   <footer>
+    @include('layouts.footer')
+   </footer>
+</body>
+</html>
