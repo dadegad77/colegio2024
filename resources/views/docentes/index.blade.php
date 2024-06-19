e271b4a57705d6da79c7237b8611b829d683ba08
diegohenao <dadegad@gmail.com>
2024-06-19 10:48:20 -0500

    creacion usuarios

--- resources/views/docentes/index.blade.php
+++ resources/views/docentes/index.blade.php
@@ -5,11 +5,11 @@
 <h1 class="text-3xl text-green-500 font-bold underline text-center ">PORTAL DOCENTE</h1>
 
 <div class="card-actions justify-end">
-<a href="docentes/create" class=" d-sm-inline-flex-w-16 d-md-inline-flex-w-32 d-lg-inline-flex-w-48 mx-4 items-center justify-center px-12 py-4 text-base font-medium text-center text-black bg-green-200 rounded-lg hover:bg-gray-600 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-900"> INGRESAR DOCENTE </a></div>
+<a href="docentes/create" class=" d-sm-inline-flex-w-16 d-md-inline-flex-w-32 d-lg-inline-flex-w-48 mx-4 items-center justify-center px-12 py-4 text-base font-medium text-center text-black bg-green-200 rounded-lg hover:bg-gray-600 focus:ring-4 focus:ring-gray-300"> INGRESAR DOCENTE </a></div>
 <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 m-6">
      @foreach ($docentes as $docente)
         <div class="card w-60 bg-base-200 shadow-xl">
-          <figure><img src="https://source.unsplash.com/random/800x600/?person&{{ $docente->nombre}}" alt="{{ $docente->nombre}}"/></figure>
+          <figure><img src="https://loremflickr.com/200/200/store&{{ $docente->nombre}}" alt="{{ $docente->nombre}}"/></figure>
           <div class="card-body">
              <h2 class="card-title">
                 {{ $docente->nombre }}
